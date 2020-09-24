5156488cb9fc1a8ea06168a0c93f9252d14400c7
Srujan Kumar Bojjam <srujan@swecha.net>
2020-09-25 03:23:40 +0530

    Introduce Price in Product

--- src/main/java/com/tw/dddworkshop/domain/Product.java
+++ src/main/java/com/tw/dddworkshop/domain/Product.java
@@ -5,9 +5,19 @@ import java.util.Objects;
 public class Product {
 
     private String name;
+    private Price price;
 
-    public Product(String name) {
+    public Product(String name, Price price) {
         this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Price getPrice() {
+        return price;
     }
 
     @Override
@@ -15,18 +25,20 @@ public class Product {
         if (this == o) return true;
         if (!(o instanceof Product)) return false;
         Product product = (Product) o;
-        return name.equals(product.name);
+        return Objects.equals(name, product.name) &&
+                Objects.equals(price, product.price);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name);
+        return Objects.hash(name, price);
     }
 
     @Override
     public String toString() {
         return "Product{" +
                 "name='" + name + '\'' +
+                ", price=" + price +
                 '}';
     }
 }
--- src/test/java/com/tw/dddworkshop/domain/CartTest.java
+++ src/test/java/com/tw/dddworkshop/domain/CartTest.java
@@ -4,6 +4,9 @@ import com.tw.dddworkshop.domain.events.ItemAddedToCartEvent;
 import com.tw.dddworkshop.domain.events.ItemRemovedFromCartEvent;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.util.Currency;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -13,7 +16,7 @@ public class CartTest {
     @Test
     public void shouldAddProductToTheCart() {
         Cart cart = new Cart();
-        Product product = new Product("IPad Pro");
+        Product product = new Product("IPad Pro", new Price(new BigDecimal(100), Currency.getInstance("INR")));
         Item item = new Item(product, 1);
 
         cart.addItem(item);
@@ -24,10 +27,10 @@ public class CartTest {
     @Test
     public void shouldAddSecondProductToTheCart() {
         Cart cart = new Cart();
-        Product product1 = new Product("IPad Pro");
+        Product product1 = new Product("IPad Pro", new Price(new BigDecimal(100), Currency.getInstance("INR")));
         Item item1 = new Item(product1, 1);
         cart.addItem(item1);
-        Product product2 = new Product("Hero Ink Pen");
+        Product product2 = new Product("Hero Ink Pen", new Price(new BigDecimal(100), Currency.getInstance("INR")));
 
         Item item2 = new Item(product2, 1);
         cart.addItem(item2);
@@ -40,53 +43,54 @@ public class CartTest {
     @Test
     public void shouldAddAProductWithQuantityMoreThanOne() {
         Cart cart = new Cart();
-        Product product = new Product("GM Cricket Bat");
+        Product product = new Product("GM Cricket Bat", new Price(new BigDecimal(100), Currency.getInstance("INR")));
 
         Item item = new Item(product, 2);
         cart.addItem(item);
 
-        assertTrue(cart.getItems().get(0).getProduct().toString().contains("GM Cricket Bat"));
+        assertEquals(cart.getItems().get(0).getProduct().getName(),"GM Cricket Bat");
+        assertEquals(cart.getItems().get(0).getProduct().getPrice(),new Price(new BigDecimal(100), Currency.getInstance("INR")));
         assertEquals(cart.getItems().get(0).getQuantity(), 2);
     }
 
     @Test
     public void shouldRemoveItem() {
         Cart cart = new Cart();
-        Product product1 = new Product("Hero Ink Pen");
+        Product product1 = new Product("Hero Ink Pen", new Price(new BigDecimal(100), Currency.getInstance("INR")));
         Item item1 = new Item(product1, 1);
         cart.addItem(item1);
-        Product product2 = new Product("GM Cricket Bat");
+        Product product2 = new Product("GM Cricket Bat", new Price(new BigDecimal(100), Currency.getInstance("INR")));
         Item item2 = new Item(product2, 2);
         cart.addItem(item2);
 
-        Item itemToBeRemoved = new Item(new Product("Hero Ink Pen"), 1);
+        Item itemToBeRemoved = new Item(new Product("Hero Ink Pen", new Price(new BigDecimal(100), Currency.getInstance("INR"))), 1);
         cart.removeItem(itemToBeRemoved);
 
         assertEquals(cart.getItems().size(), 1);
-        assertTrue(cart.getItems().get(0).getProduct().toString().contains("GM Cricket Bat"));
+        assertEquals(cart.getItems().get(0).getProduct().getName(), "GM Cricket Bat");
         assertEquals(cart.getItems().get(0).getQuantity(), 2);
     }
 
     @Test
     public void shouldAddItemAddedToCartEventWhenItemIsAdded() {
         Cart cart = new Cart();
-        Product product = new Product("GM Cricket Bat");
+        Product product = new Product("GM Cricket Bat", new Price(new BigDecimal(100), Currency.getInstance("INR")));
 
         Item item = new Item(product, 2);
         cart.addItem(item);
 
         assertEquals(cart.getItems().size(), 1);
-        assertTrue(cart.getItems().get(0).getProduct().toString().contains("GM Cricket Bat"));
+        assertEquals(cart.getItems().get(0).getProduct().getName(), "GM Cricket Bat");
         assertEquals(cart.getItems().get(0).getQuantity(), 2);
         assertEquals(cart.getEvents().size(), 1);
-        assertTrue(( (ItemAddedToCartEvent) cart.getEvents().get(0)).getItem().getProduct().toString().contains("GM Cricket Bat"));
+        assertEquals(( (ItemAddedToCartEvent) cart.getEvents().get(0)).getItem().getProduct().getName(), "GM Cricket Bat");
         assertEquals(( (ItemAddedToCartEvent) cart.getEvents().get(0)).getItem().getQuantity(), 2);
     }
 
     @Test
     public void shouldAddItemRemovedFromCartEventWhenItemIsRemoved() {
         Cart cart = new Cart();
-        Product product = new Product("GM Cricket Bat");
+        Product product = new Product("GM Cricket Bat", new Price(new BigDecimal(100), Currency.getInstance("INR")));
         Item item = new Item(product, 2);
         cart.addItem(item);
 
@@ -96,9 +100,9 @@ public class CartTest {
 
         assertEquals(cart.getItems().size(), 0);
         assertEquals(cart.getEvents().size(), 2);
-        assertTrue(( (ItemAddedToCartEvent) cart.getEvents().get(0)).getItem().getProduct().toString().contains("GM Cricket Bat"));
+        assertEquals(( (ItemAddedToCartEvent) cart.getEvents().get(0)).getItem().getProduct().getName(), "GM Cricket Bat");
         assertEquals(( (ItemAddedToCartEvent) cart.getEvents().get(0)).getItem().getQuantity(), 2);
-        assertTrue(( (ItemRemovedFromCartEvent) cart.getEvents().get(1)).getItem().getProduct().toString().contains("GM Cricket Bat"));
+        assertEquals(( (ItemRemovedFromCartEvent) cart.getEvents().get(1)).getItem().getProduct().getName(), "GM Cricket Bat");
         assertEquals(( (ItemRemovedFromCartEvent) cart.getEvents().get(1)).getItem().getQuantity(), 2);
     }
 
@@ -106,7 +110,7 @@ public class CartTest {
     public void shouldDifferentiateCartsOfDifferentUsers() {
         Cart cart1 = new Cart();
         Cart cart2 = new Cart();
-        Product product = new Product("GM Cricket Bat");
+        Product product = new Product("GM Cricket Bat", new Price(new BigDecimal(100), Currency.getInstance("INR")));
         Item item = new Item(product, 2);
 
         cart1.addItem(item);
